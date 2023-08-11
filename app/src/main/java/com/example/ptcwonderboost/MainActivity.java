ea6ad9332b2e691aacbca311fe7949f373ac4733
JuanPabloFloresDiaz <20220599@ricaldone.edu.sv>
2023-08-10 19:02:53 -0600

    PrimerAvance

---
+++ app/src/main/java/com/example/ptcwonderboost/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.ptcwonderboost;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Handler;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.util.Timer;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {Intent intent = new Intent(MainActivity.this, Login.class);
+               startActivity(intent);
+               finish();
+            }
+        }, 3900);
+
+    }
+}
