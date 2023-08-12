e18e3d961ea2d89a40c53487ecc9c4c6aa1e331b
JuanPabloFloresDiaz <20220599@ricaldone.edu.sv>
2023-08-12 03:21:30 -0600

    Creación de metodo de conexión

--- app/src/main/java/com/example/ptcwonderboost/Login.java
+++ app/src/main/java/com/example/ptcwonderboost/Login.java
@@ -1,14 +1,17 @@
 package com.example.ptcwonderboost;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.sql.Connection;
 public class Login extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +24,28 @@ public class Login extends AppCompatActivity {
                 testDatabaseConnection();
             }
         });
-    }
+
+        TextView registrarse = findViewById(R.id.twcuenta);
+            registrarse.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(Login.this, Registrarse.class);
+                    startActivity(intent);
+                }
+            });
+
+           TextView recuperacion = findViewById(R.id.twclave);
+           recuperacion.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+                   Intent intent = new Intent(Login.this, ReestablecerClave.class);
+                   startActivity(intent);
+               }
+            });
+        }
+
+
+
 
     private void testDatabaseConnection() {
         Connection connection = Conexion.getConnection(this);
@@ -32,4 +56,6 @@ public class Login extends AppCompatActivity {
             Toast.makeText(this, "Error en la conexión", Toast.LENGTH_SHORT).show();
         }
     }
+
+
 }
