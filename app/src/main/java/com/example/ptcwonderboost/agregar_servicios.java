31d77de33cd2db9443776cd944ed28075e3328e7
GersonNerio <82895841+Gerson503@users.noreply.github.com>
2023-08-22 08:07:08 -0600

    funciona el boton agregar foto de servicios

--- app/src/main/java/com/example/ptcwonderboost/agregar_servicios.java
+++ app/src/main/java/com/example/ptcwonderboost/agregar_servicios.java
@@ -1,6 +1,7 @@
 package com.example.ptcwonderboost;
 
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -22,13 +23,44 @@ import java.util.List;
 
 public class agregar_servicios extends AppCompatActivity {
 
+
+    private Button btnSeleimg;
+
+    private ImageView imgProd;
+
+    protected static final int REQUEST_CODE_IMAGE = 101;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_servicios);
 
+        btnSeleimg = findViewById(R.id.btnSeleImg);
+        imgProd = findViewById(R.id.imgProd);
 
 
+        btnSeleimg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openImagePicker();
+            }
+        });
+
+
+    }
+    private void openImagePicker() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, REQUEST_CODE_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri imageUri = data.getData();
+            imgProd.setImageURI(imageUri);
+        }
     }
 }
 
