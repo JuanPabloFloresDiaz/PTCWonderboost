3efdbad86c65c88fb2521daf7f212629f0e44ee1
JuanPabloFloresDiaz <20220599@ricaldone.edu.sv>
2023-09-25 00:33:37 -0600

    AVANCES DEFENSA 100% PARTE 4

--- app/src/main/java/com/example/ptcwonderboost/PagoSeguimientos.java
+++ app/src/main/java/com/example/ptcwonderboost/PagoSeguimientos.java
@@ -1,5 +1,6 @@
 package com.example.ptcwonderboost;
 
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -7,9 +8,11 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -21,6 +24,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.math.BigDecimal;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
@@ -28,10 +32,15 @@ import java.util.List;
 public class PagoSeguimientos extends AppCompatActivity {
     private ListView listView;
     private Spinner formaPago;
-
+    private EditText fechaEditText;
+    private int idFormaPago;
     private List<String> datos;
     private Button btnGuardar;
+    private Calendar calendar = Calendar.getInstance();
 
+    public final void GuardarPago(){
+
+    }
     public final void LeerDatos(){
         Pagos pagos = new Pagos();
         pagos.setIdVendedor(VariablesGlobales.getIdPersona());
@@ -74,12 +83,98 @@ public class PagoSeguimientos extends AppCompatActivity {
 
     }
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pago_seguimientos);
         listView = findViewById(R.id.listViewSeguimientoPago);
         LeerDatos();
+        Pagos pagos = new Pagos();
+        formaPago = findViewById(R.id.spinnerEstadoPago);
+        ResultSet rs = pagos.CargarEstadoPago();
+        if (rs != null) {
+            List<String> datos1 = new ArrayList<>();
+            datos1.add("Seleccionar");
+            try {
+                while (rs.next()) {
+                    String nacionalidad = rs.getString("EstadoPago");
+                    datos1.add(nacionalidad);
+                }
+                // Crea el adaptador y asigna los datos al Spinner
+                ArrayAdapter<String> adapterN1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, datos1);
+                adapterN1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                formaPago.setAdapter(adapterN1);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        formaPago.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = (String) parent.getItemAtPosition(position);
+                if (!selectedItem.equals("Seleccionar")) {
+                    // Realiza acciones basadas en el elemento seleccionado
+                    idFormaPago = (int) id;
+                }else{
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Manejo cuando no se selecciona nada
+            }
+        });
+        fechaEditText = findViewById(R.id.txtFechaPago);
+        fechaEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePicker();
+            }
+        });
+        btnGuardar = findViewById(R.id.btnGuardarPago);
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    GuardarPago();
+                }catch (Exception ex){
+
+                }
+            }
+        });
     }
+
+    private void showDatePicker() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                dateSetListener,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+        );
+        datePickerDialog.show();
+    }
+
+    private DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            Calendar currentDate = Calendar.getInstance();
+            Calendar minDate = Calendar.getInstance();
+            minDate.set(1900, Calendar.JANUARY, 1);
+            Calendar selectedDate = Calendar.getInstance();
+            selectedDate.set(year, month, dayOfMonth);
+
+            if (selectedDate.after(currentDate)) {
+                Toast.makeText(PagoSeguimientos.this, "Seleccione una fecha menor o igual a la fecha actual", Toast.LENGTH_SHORT).show();
+            } else if (selectedDate.before(minDate)) {
+                Toast.makeText(PagoSeguimientos.this, "Seleccione una fecha mayor a 1900", Toast.LENGTH_SHORT).show();
+            } else {
+                String selectedDateStr = dayOfMonth + "/" + (month + 1) + "/" + year;
+                fechaEditText.setText(selectedDateStr);
+            }
+        }
+    };
 
 }
--- app/src/main/java/com/example/ptcwonderboost/Pagos.java
+++ app/src/main/java/com/example/ptcwonderboost/Pagos.java
@@ -7,13 +7,39 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 public class Pagos {
     private int idVendedor;
-
+    private int idEstadoPago;
+    private int idEnvio;
+    private String fechaPago;
     public int getIdVendedor() {
         return idVendedor;
     }
 
     public void setIdVendedor(int idVendedor) {
         this.idVendedor = idVendedor;
+    }
+
+    public int getIdEstadoPago() {
+        return idEstadoPago;
+    }
+
+    public void setIdEstadoPago(int idEstadoPago) {
+        this.idEstadoPago = idEstadoPago;
+    }
+
+    public int getIdEnvio() {
+        return idEnvio;
+    }
+
+    public void setIdEnvio(int idEnvio) {
+        this.idEnvio = idEnvio;
+    }
+
+    public String getFechaPago() {
+        return fechaPago;
+    }
+
+    public void setFechaPago(String fechaPago) {
+        this.fechaPago = fechaPago;
     }
 
     //Cargar tabla de seguimiento de pagos
@@ -34,16 +60,43 @@ public class Pagos {
         }
     }
 
-    public ResultSet CargarFormaPago(){
+    public ResultSet CargarEstadoPago(){
         PreparedStatement ps;
         Connection con;
         try {
             con = Conexion.getConnection(null);
-            ps = con.prepareStatement("SELECT * FROM tbFormasDePagos");
+            ps = con.prepareStatement("SELECT * FROM tbEstadosPago");
             ResultSet respuesta=ps.executeQuery();
             return respuesta;
         } catch (Exception e) {
             return null;
         }
     }
+
+    public int IngresarPago(){
+        PreparedStatement ps;
+        Connection con = null;
+        try{
+            con = Conexion.getConnection(null);
+            String query = "INSERT INTO tbSeguimientosPago(idEstadosPago, fechaPago, idEnvio) VALUES (?,?,?)";
+            ps = con.prepareStatement(query);
+            ps.setInt(1, idEstadoPago);
+            ps.setString(2, fechaPago);
+            ps.setInt(3, idEnvio);
+            ps.execute();
+            return 1;
+        }catch(Exception e){
+            return 0;
+        }finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
 }
